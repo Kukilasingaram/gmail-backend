cb1c8ecb368016b4064de08c190e3b8f2db5d272
kukila <143105348+Kukilasingaram@users.noreply.github.com>
2024-03-23 14:04:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { dbConnection } from "./database/db.js";
-import routes from "./routes/route.js";
+import express from 'express';
+import cors from 'cors';
+import Connection from './database/db.js';
+import routes from './routes/route.js';
 
 const app = express();
 
-// Db connection
-dbConnection();
-
-dotenv.config();
-
-const Port = process.env.PORT;
-
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 app.use(express.json());
+app.use('/', routes);
 
-app.use("/", routes);
-
-app.get("/", (req, res) => {
-  res.send("Connected successfully");
+app.get("/",(req,res)=>{
+    res.send("Connected Successfully")
 });
 
-app.listen(Port, () =>
-  console.log(`The server connected in local host : ${Port}`)
-);
+const PORT = 8000;
+
+Connection();
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
